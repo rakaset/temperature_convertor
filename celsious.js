db0fd318f2b23c88c6052d0f6113aefee7ef9a34
rakaset <147502511+rakaset@users.noreply.github.com>
2023-10-20 19:33:11 +0530

    Add files via upload

---
+++ celsious.js
@@ -0,0 +1,10 @@
+const celsiusInput = document.getElementById('celsius');
+const fahrenheitInput = document.getElementById('fahrenheit');
+const convertButton = document.getElementById('convert');
+
+convertButton.addEventListener('click', () => {
+  const celsius = parseFloat(celsiusInput.value);
+  const fahrenheit = (celsius * 9 / 5) + 32;
+
+  fahrenheitInput.value = fahrenheit;
+});
